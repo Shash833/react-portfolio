4463c4f4545431073b371ddd2c17a549e547e05c
Shash833 <58815647+Shash833@users.noreply.github.com>
2020-08-04 23:49:54 +1000

    update information

--- src/pages/about.js
+++ src/pages/about.js
@@ -18,14 +18,13 @@ function AboutMe() {
                 <Image src={photo} />
             </Column>
             <Column size={8}>
-                <p> I am a podiatrist, currently in my fifth year of practice. After completing a Bachelor
-                of Health Sciences and Master in Podiatric Practice in 2015, I gained experience in the
-                public and private health sector.
-                <br />
-                    <br />
-                I am currently enrolled in a part-time full stack web development bootcamp with Trilogy
-                Education and Monash University to study coding which I have chosen to explore as a personal
-                challenge and a potential career pathway in the future. During the bootcamp, I have come to thoroughly enjoy the statisfaction of seeing code come together to create websites.
+                <p>
+                    I am a Full Stack Web Developer, recently having completed a Coding Bootcamp with Trilogy
+                    Education and Monash University. During the bootcamp, I have come to thoroughly enjoy the statisfaction of solving problems and seeing code come together to create websites and I am excited to further develop my skills and explore a furture career as Web Developer.
+                </p>
+                <p>
+                    I am currently working as a podiatrist, in my fifth year of practice, after completing a Bachelor of Health Sciences and Master in Podiatric Practice in 2015.
+                    In my spare time I enjoy cooking, reading, board games and travelling.
                 </p>
                 <p><b>Extra information:</b></p>
                 <p><a href={resume}>Current resume (pdf)</a></p>
