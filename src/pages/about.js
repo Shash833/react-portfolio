44b253cbcd90fac5392ade1ff9edce28a43d31a0
Shash833 <58815647+Shash833@users.noreply.github.com>
2020-09-08 01:41:21 +1000

    update contact info

--- src/pages/about.js
+++ src/pages/about.js
@@ -18,14 +18,17 @@ function AboutMe() {
                 <Image src={photo} />
             </Column>
             <Column size={8}>
-                <p>
-                    I am a Full Stack Web Developer, recently having completed a Coding Bootcamp with Trilogy
-                    Education and Monash University. During the bootcamp, I have come to thoroughly enjoy the statisfaction of solving problems and seeing code come together to create websites and I am excited to further develop my skills and explore a furture career as Web Developer.
+                <div id="about-me">
+                    <p>
+                        I am a Full Stack Web Developer, recently having completed a Coding Bootcamp with Trilogy
+                        Education and Monash University. During the bootcamp, I have come to thoroughly enjoy the statisfaction of solving problems and seeing code come together to create websites and I am excited to further develop my skills and explore a furture career as Web Developer.
                 </p>
-                <p>
-                    I am currently working as a podiatrist, in my fifth year of practice, after completing a Bachelor of Health Sciences and Master in Podiatric Practice in 2015.
-                    In my spare time I enjoy cooking, reading, board games and travelling.
+                    <p>
+                        I am currently working as a podiatrist, in my fifth year of practice, after completing a Bachelor of Health Sciences and Master in Podiatric Practice in 2015.
+                        In my spare time I enjoy cooking, reading, board games and travelling.
                 </p>
+                </div>
+
             </Column>
         </Row>
     </Container>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,16 +10,18 @@ function Contact() {
         <Row>
             <Title>Contact</Title>
         </Row>
+        <hr></hr>
         <Row>
-        <p><b>Email:</b> shashini.mayadunne@gmail.com </p>
-        <p>Git Hub: <a target = "_blank" href="https://github.com/Shash833">https://github.com/Shash833</a> </p>
-        <p>LinkedIn: <a target = "_blank" href="https://www.linkedin.com/in/shashini-mayadunne/">https://www.linkedin.com/in/shashini-mayadunne</a>
+            <div id="contact-info">
+                <p><b>Email:</b> shashini.mayadunne@gmail.com </p>
+                <p><b>Git Hub: </b><a target="_blank" href="https://github.com/Shash833">https://github.com/Shash833</a> </p>
+                <p><b>LinkedIn:</b> <a target="_blank" href="https://www.linkedin.com/in/shashini-mayadunne/">https://www.linkedin.com/in/shashini-mayadunne</a>
+                </p>
+            </div>
+        </Row>
+        <hr></hr>
+        <p className="formHeading"><b>Please email any enquiries through this form and I will respond as soon as possible:</b>
         </p>
-        </Row>
-        <Row>
-            <p className="formHeading">Please email any enquiries through this form and I will respond as soon as possible.
-            </p>
-        </Row>
         <ContactForm />
     </Container>
 }
